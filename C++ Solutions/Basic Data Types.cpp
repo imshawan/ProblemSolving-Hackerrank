9c3e3cb12d6bcd72a2a9808609041b9be67ac85e
Shawan Mandal <shawan.sm049@gmail.com>
2021-05-05 22:06:25 +0530

    Solutions for c++

---
+++ C++ Solutions/Basic Data Types.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a;
+    long b;
+    char ch;
+    float f;
+    double di;
+    
+    scanf("%d %ld %c %f %lf", &a,&b,&ch,&f,&di);
+    
+    printf("%d\n", a);
+    printf("%ld\n", b);
+    printf("%c\n", ch);
+    printf("%f\n", f);
+    printf("%lf", di);
+    
+    return 0;
+}
